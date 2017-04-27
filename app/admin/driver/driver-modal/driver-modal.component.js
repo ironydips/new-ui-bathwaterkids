46f92b882c41acb2dffbeaba495f797512aee872
SupriyaSgn <supriyasingh.ggit@gmail.com>
2017-04-27 19:56:00 +0530

    modifications in Admin module

--- app/admin/driver/driver-modal/driver-modal.component.js
+++ app/admin/driver/driver-modal/driver-modal.component.js
@@ -1,36 +1,51 @@
 (function(angular) {
-'use strict';
+    'use strict';
 
-function DriverModalController($state, DriverService) {
-	var ctrl = this;
+    function DriverModalController($state, DriverService) {
+    	var ctrl = this;
 
-	ctrl.driver = (ctrl.resolve && ctrl.resolve.details) || {};
-	ctrl.isDisabled = Object.keys(ctrl.driver).length > 0;
-	//Add Driver
-	ctrl.save = function(valid){  
-		console.log(valid)
+        ctrl.init = function() {
+                
+                ctrl.driver = (ctrl.resolve && ctrl.resolve.details) || {};
+                ctrl.isDisabled = Object.keys(ctrl.driver).length > 0;
+            }
+            //Add Driver
+        ctrl.save = function() {
 
-		DriverService.addDriver(ctrl.driver)
-		.then(function(result){
-			ctrl.modalInstance.close({action: 'update'});
-		})
-		.catch(function(err){
-			console.log('Error Adding Driver');
-			console.log(err);
-		});
-	};
-	ctrl.cancel = function(){
-		ctrl.modalInstance.close();
-	}
-}
+            angular.forEach(ctrl.driverDetailForm.$error.required, function(field) {
+                field.$setDirty();
+            });
+            if (!ctrl.driverDetailForm.$invalid) {
 
-angular.module('driverModal')
-	.component('driverModal',{
-		templateUrl: 'admin/driver/driver-modal/driver-modal.template.html',
-		controller:['$state','DriverService', DriverModalController],
-		bindings:{
-			modalInstance: '<',
-			resolve: '<'
-		}
-	});
+                DriverService.addDriver(ctrl.driver)
+                    .then(function(result) {
+                        ctrl.modalInstance.close({ action: 'update' });
+                    })
+                    .catch(function(err) {
+                        console.log('Error Adding Driver');
+                        console.log(err);
+                    });
+            }
+        };
+
+        ctrl.showMessage = function(input) {
+            var show = input.$invalid && (input.$dirty || input.$touched);
+            return show;
+        };
+        ctrl.cancel = function() {
+            ctrl.modalInstance.close();
+        };
+
+        ctrl.init();
+    }
+
+    angular.module('driverModal')
+        .component('driverModal', {
+            templateUrl: 'admin/driver/driver-modal/driver-modal.template.html',
+            controller: ['$state', 'DriverService', DriverModalController],
+            bindings: {
+                modalInstance: '<',
+                resolve: '<'
+            }
+        });
 })(window.angular);
--- app/admin/truck/truck-modal/truck-modal.component.js
+++ app/admin/truck/truck-modal/truck-modal.component.js
@@ -1,49 +1,60 @@
 (function(angular) {
-'use strict';
+    'use strict';
 
-function TruckModalController($scope, $rootScope,$state, resizeService, TruckService) {
-	var ctrl = this;
-	ctrl.truck = (ctrl.resolve && ctrl.resolve.details) || {};
-	ctrl.isDisabled = Object.keys(ctrl.truck).length > 0;
-	if(ctrl.truck.images && ctrl.truck.images.length > 0){
-		ctrl.imageUrl = ctrl.truck.images[0].url;
-	}
+    function TruckModalController($scope, $rootScope, $state, resizeService, TruckService) {
+        var ctrl = this;
+        ctrl.truck = (ctrl.resolve && ctrl.resolve.details) || {};
+        ctrl.isDisabled = Object.keys(ctrl.truck).length > 0;
+        if (ctrl.truck.images && ctrl.truck.images.length > 0) {
+            ctrl.imageUrl = ctrl.truck.images[0].url;
+        }
 
-	// Watch the image change and show from base 64 value
-	// $scope is only used here for watch.
-	$scope.$watch(angular.bind(ctrl, function(){
-		return ctrl.selectedImage;
-	}), function(value){
-		value ? 
-			(ctrl.imageUrl = 'data:image/jpeg;base64, ' + value.base64, ctrl.truck.truckImage = value.base64)
-			: (ctrl.truck.truckImage = '');
-	});
+        // Watch the image change and show from base 64 value
+        // $scope is only used here for watch.
+        $scope.$watch(angular.bind(ctrl, function() {
+            return ctrl.selectedImage;
+        }), function(value) {
+            value ?
+                (ctrl.imageUrl = 'data:image/jpeg;base64, ' + value.base64, ctrl.truck.truckImage = value.base64) : (ctrl.truck.truckImage = '');
+        });
 
-	ctrl.save = function(){
+        ctrl.save = function() {
 
-		TruckService.addTruckwithImage(ctrl.truck)
-			.then(function(result){
-				ctrl.modalInstance.close({action: 'update'});
-		})
-		.catch(function(err){
-			console.log('Error Adding Truck');
-			console.log(err);
-		});
-	}
+            angular.forEach(ctrl.truckDetailForm.$error.required, function(field) {
+                field.$setDirty();
+            });
+            if (!ctrl.truckDetailForm.$invalid) {
 
-	ctrl.cancel = function(){
-		ctrl.modalInstance.close();
-	}
-}
+                TruckService.addTruckwithImage(ctrl.truck)
+                    .then(function(result) {
+                        ctrl.modalInstance.close({ action: 'update' });
+                    })
+                    .catch(function(err) {
+                        console.log('Error Adding Truck');
+                        console.log(err);
+                    });
+            }
 
-angular.module('truckModal')
-	.component('truckModal',{
-		templateUrl: 'admin/truck/truck-modal/truck-modal.template.html',
-		controller:['$scope','$rootScope','$state', 'resizeService','TruckService', TruckModalController],
-		bindings:{
-			modalInstance: '<',
-			resolve: '<'
-		}
-	});
+        };
+
+        ctrl.showMessage = function(input) {
+            var show = input.$invalid && (input.$dirty || input.$touched);
+            return show;
+        };
+
+        ctrl.cancel = function() {
+            ctrl.modalInstance.close();
+        }
+    }
+
+    angular.module('truckModal')
+        .component('truckModal', {
+            templateUrl: 'admin/truck/truck-modal/truck-modal.template.html',
+            controller: ['$scope', '$rootScope', '$state', 'resizeService', 'TruckService', TruckModalController],
+            bindings: {
+                modalInstance: '<',
+                resolve: '<'
+            }
+        });
 
 })(window.angular);
