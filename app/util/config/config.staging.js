aa9b960316e7949d5eaaf7b970ed24bb361114ca
SupriyaSgn <supriyasingh.ggit@gmail.com>
2018-01-25 22:39:16 +0530

    End Point Change

--- app/util/config/config.staging.js
+++ app/util/config/config.staging.js
@@ -1,6 +1,6 @@
 (function (angular) {
 	angular.config = {
-		baseUrl: "http://net.site4demo.com:8222/BathwaterKids",
+		baseUrl: "http://bathwater-staging.us-east-1.elasticbeanstalk.com",
 		clientID: "531188035829-e83na2d4nj50fu7baqc7q7mlqauboqvs.apps.googleusercontent.com"
 	}
 })(window.angular)
